2a0405bbc98c518e7eff4859170abd9a58349726
Paulo Dichone <pdichone@gmail.com>
2014-11-29 15:51:29 -0800

    Create details screen and added the appropriate manifest permissions

---
+++ app/src/main/java/twitterap/magadistudio/com/twitterapp/util/LruBitmapCache.java
@@ -0,0 +1,46 @@
+package twitterap.magadistudio.com.twitterapp.util;
+
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.LruCache;
+
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
+public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageCache {
+	
+	public static int getDefaultLruCacheSize(){
+		final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+		final int cacheSize = maxMemory / 8;
+		
+		return cacheSize;
+	}
+	
+	
+	public LruBitmapCache(){
+		this(getDefaultLruCacheSize());
+	}
+	
+	public LruBitmapCache(int sizeInKiloBytes){
+		super(sizeInKiloBytes);
+	}
+	
+	@Override
+	protected int sizeOf(String key, Bitmap value){
+		return value.getRowBytes() * value.getHeight() / 1024;
+	}
+
+	@Override
+	public Bitmap getBitmap(String url) {
+		// TODO Auto-generated method stub
+		return get(url);
+	}
+
+	@Override
+	public void putBitmap(String url, Bitmap bitmap) {
+		
+		put(url, bitmap);
+	}
+
+}
